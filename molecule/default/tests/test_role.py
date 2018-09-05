93fbcd589042657d209303048a4f04ab3e016329
John Freeman <john.freeman.opensource@gmail.com>
2018-09-05 12:14:45 +0100

    Changed tests to use host fixture (#6)
    Other fixtures are deprecated and will be removed in 2.0 Testinfra release.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -15,18 +15,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     'libexec',
     'jre',
 ])
-def test_directories(Command, File, dir_name):
+def test_directories(host, dir_name):
     install_dir_pattern = '/opt/git-credential-manager/[0-9\\.]+$'
-    install_dir = Command.check_output('find %s | grep --color=never -E %s',
-                                       '/opt/git-credential-manager',
-                                       install_dir_pattern)
-    dir = File(install_dir)
+    install_dir = host.check_output('find %s | grep --color=never -E %s',
+                                    '/opt/git-credential-manager',
+                                    install_dir_pattern)
+    dir = host.file(install_dir)
     assert dir.exists
     assert dir.is_directory
     assert dir.user == 'root'
     assert dir.group == 'root'
 
-    dir = File(install_dir + '/' + dir_name)
+    dir = host.file(install_dir + '/' + dir_name)
     assert dir.exists
     assert dir.is_directory
     assert dir.user == 'root'
@@ -37,53 +37,53 @@ def test_directories(Command, File, dir_name):
     'bin/git-credential-manager',
     'jre/bin/java',
 ])
-def test_files(Command, File, file_path):
+def test_files(host, file_path):
     install_dir_pattern = '/opt/git-credential-manager/[0-9\\.]+$'
-    install_dir = Command.check_output('find %s | grep --color=never -E %s',
-                                       '/opt/git-credential-manager',
-                                       install_dir_pattern)
-    dir = File(install_dir)
+    install_dir = host.check_output('find %s | grep --color=never -E %s',
+                                    '/opt/git-credential-manager',
+                                    install_dir_pattern)
+    dir = host.file(install_dir)
     assert dir.exists
     assert dir.is_directory
     assert dir.user == 'root'
     assert dir.group == 'root'
 
-    installed_file = File(install_dir + '/' + file_path)
+    installed_file = host.file(install_dir + '/' + file_path)
     assert installed_file.exists
     assert installed_file.is_file
     assert installed_file.user == 'root'
     assert installed_file.group == 'root'
 
 
-def test_libexec(Command, File):
+def test_libexec(host):
     file_pattern = ('/opt/git-credential-manager/[0-9\\.]+/libexec/'
                     'git-credential-manager-[0-9\\.]+\\.jar$')
-    file_path = Command.check_output('find %s | grep --color=never -E %s',
-                                     '/opt/git-credential-manager',
-                                     file_pattern)
-    installed_file = File(file_path)
+    file_path = host.check_output('find %s | grep --color=never -E %s',
+                                  '/opt/git-credential-manager',
+                                  file_pattern)
+    installed_file = host.file(file_path)
     assert installed_file.exists
     assert installed_file.is_file
     assert installed_file.user == 'root'
     assert installed_file.group == 'root'
 
 
-def test_link(Command, File):
-    installed_file = File('/usr/local/bin/git-credential-manager')
+def test_link(host):
+    installed_file = host.file('/usr/local/bin/git-credential-manager')
     assert installed_file.exists
     assert installed_file.is_symlink
     assert installed_file.user == 'root'
     assert installed_file.group == 'root'
 
 
-def test_version(Command, File):
-    version = Command.check_output('git-credential-manager version')
+def test_version(host):
+    version = host.check_output('git-credential-manager version')
     pattern = 'Git Credential Manager for Mac and Linux version [0-9\\.]'
     assert re.match(pattern, version)
 
 
-def test_git_config(Command, File):
-    config = Command.check_output('git config --system credential.helper')
+def test_git_config(host):
+    config = host.check_output('git config --system credential.helper')
     pattern = ("!'?/opt/git-credential-manager/[0-9\\.]+/jre/bin/java'?"
                " -Ddebug=false -Djava.net.useSystemProxies=true"
                " -Xshareclasses:name=git-credential-manager -Xquickstart"
